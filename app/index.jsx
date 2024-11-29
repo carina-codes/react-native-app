eb78c76404d7c73efdfb431356a22a3964dcc485
Carina Rivera <crivera@nomnomdata.com>
2024-11-29 15:31:54 -0800

    added global provider and wrapped up user login logic

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar'; 
-import { router } from 'expo-router';
+import { router, Redirect } from 'expo-router';
 import { View, Text, Image, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../constants";
 import CustomButton from "../components/CustomButton";
+import { useGlobalContext } from '../context/GlobalProvider';
 
 export default function App() {
+  const { isLoading, isLoggedIn } = useGlobalContext();
+
+  if(!isLoading && isLoggedIn) return <Redirect href="/home" />
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: '100%' }}>
